1e36ed7f7f3b7a41df4213594571899f1370d1be
hana222444 <hanar6stake@gmail.com>
2024-11-12 22:54:38 +0900

    Lettle_Header追加

--- wakarun/app/auth/signup/page.tsx
+++ wakarun/app/auth/signup/page.tsx
@@ -1,6 +1,9 @@
+import Little_Header from "@/app/components/layout/Little_Header";
+
 export default function Page() {
   return (
     <>
+      <Little_Header/>
       <h1>Hello, signup!</h1>
     </>
   );
