bec84dc953ea62c6f02b4f8de3ca2fe628d02e51
hana222444 <hanar6stake@gmail.com>
2024-11-30 00:44:49 +0900

    サインアウト実装など

--- wakarun/app/components/layout/header/header.tsx
+++ wakarun/app/components/layout/header/header.tsx
@@ -1,35 +1,46 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import styles from './header.module.css';
-import { FaSquarePlus } from 'react-icons/fa6';
-import { GiBookCover } from 'react-icons/gi';
-import { FaUserEdit } from 'react-icons/fa';
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./header.module.css";
+import { FaSquarePlus } from "react-icons/fa6";
+import { GiBookCover } from "react-icons/gi";
+import { FaUserEdit } from "react-icons/fa";
+import { signout } from "@/app/api/signout";
+import React from "react";
 
 const menuItems = [
   {
-    href: '/create_questions',
+    href: "/create_questions",
     icon: <FaSquarePlus size={55} />,
-    label: '問題作成',
+    label: "問題作成",
   },
   {
-    href: '/created-questions',
+    href: "/created-questions",
     icon: <GiBookCover size={55} />,
-    label: 'マイ問題集',
+    label: "マイ問題集",
   },
   {
-    href: '/my-collections',
+    href: "/my-collections",
     icon: <FaUserEdit size={55} />,
-    label: '作成した問題',
+    label: "作成した問題",
   },
 ];
+
+const handleSignout = (e: React.MouseEvent) => {
+  try {
+    signout();
+  } catch (error) {
+    e.preventDefault();
+    console.error("サインアウトエラー", error);
+  }
+};
 
 export default function Header() {
   return (
     <header className="pb-28">
       <ul className="flex">
-        <li className={`flex-1 pt-5 ${styles['header-left']}`}>
+        <li className={`flex-1 pt-5 ${styles["header-left"]}`}>
           <Link href="/" className="flex justify-start pl-14">
             <Image
               src="/images/logo.png"
@@ -41,7 +52,7 @@ export default function Header() {
         </li>
 
         {menuItems.map((item, index) => (
-          <li key={index} className={`flex-1 pt-8 ${styles['header-center']}`}>
+          <li key={index} className={`flex-1 pt-8 ${styles["header-center"]}`}>
             <Link href={item.href} className="flex justify-center">
               {item.icon}
               <span className="text-4xl pl-4 self-center">{item.label}</span>
@@ -49,8 +60,12 @@ export default function Header() {
           </li>
         ))}
 
-        <li className={`flex-1 pt-10 ${styles['header-right']}`}>
-          <Link href="/" className="flex justify-center">
+        <li className={`flex-1 pt-10 ${styles["header-right"]}`}>
+          <Link
+            href="/"
+            onClick={handleSignout}
+            className="flex justify-center"
+          >
             <span className="text-4xl">SIGN OUT</span>
           </Link>
         </li>
